a8e9757d5db37a4480aa3932d70875aab5aa7b24
Sarthakkad05 <kadsarthak33@gmail.com>
2026-05-29 16:57:46 +0530

    fix: ci workflow change

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,6 @@ output "ec2_public_ip" {
   value       = aws_instance.app_server.public_ip
 }
 
-
 output "current_ami_id" {
   description = "The AMI ID currently used by the EC2 instance"
   value       = aws_instance.app_server.ami
